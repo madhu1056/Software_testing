4e3518d5be0ec11ed6467bed171b6f50b909e47f
Madhu <madhummr2000@gmail.com>
2022-03-04 11:09:04 +0100

    adding maven files1

---
+++ Software testing Maven/src/main/java/ST_package/main_code.java
@@ -0,0 +1,10 @@
+package ST_package;
+
+public class main_code {
+    public static void main(String[] args) {
+        System.out.println("Hello Anton");
+    }
+    public int Mad(){
+        return 2;
+    }
+}
---
+++ Software testing Maven/src/test/java/ST_package/main_codeTest.java
@@ -0,0 +1,14 @@
+package ST_package;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class main_codeTest {
+    @Test
+    public void TEST_mad(){
+        main_code Class = new main_code();
+        assertEquals(2,Class.Mad());
+    }
+
+}
